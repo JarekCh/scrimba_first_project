604349187a587d4714b58c39db5d1b7cd3b8dc43
Jaroslaw Charchula <charchula@gmail.com>
2022-06-06 22:39:07 +0200

    add data file

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "06 Jan, 2022",
+        endDate: "16 Jan, 2022",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Eiffle Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/ZtnkVeLhM9MoTWLUA",
+        startDate: "10 July, 2022",
+        endDate: "26 July, 2022",
+        description: "The Eiffel Tower is a wrought iron tower that stands 1,063 ft (324 m) tall. It was designed for the Exposition Universelle, a world fair held in Paris in 1889. It is currently the most famous symbol of Paris.",
+        imageUrl: "https://www.pexels.com/photo/low-angle-photo-of-eiffel-tower-699466/"
+    },
+    {
+        title: "Niagara Falls",
+        location: "Canada",
+        googleMapsUrl: "https://goo.gl/maps/QZKvcx7ynPGE2uEu6",
+        startDate: "06 December, 2022",
+        endDate: "20 December, 2022",
+        description: "Niagara Falls, a spectacular waterfall in the NIAGARA RIVER, is the world's greatest waterfall by volume at 2832 m3. It is split in two by Goat Island. The American Falls are 59 m high (21-34 m to rock debris at the base of the falls) and 260 m wide and carry about 10% of the flow",
+        imageUrl: "https://www.pexels.com/photo/victoria-falls-306381/"
+    },
+    {
+        title: "Louvre",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/5WaN9MR8dEjZkx7P7",
+        startDate: "02 September, 2022",
+        endDate: "14 September, 2022",
+        description: "The Louvre is a museum of art and antiquities located on the Right bank of the Seine in Paris. Covering an area of 60,600 sq. m (652,300 sq. ft), and with more than 380,000 objects and 35,000 works of art, the Louvre is the largest museum in the world.",
+        imageUrl: "https://www.pexels.com/photo/clear-glass-museum-during-golden-hour-2363/"
+    },
+]
